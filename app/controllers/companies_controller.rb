aa343075fef06a4f9163e62aae711af454ad6bbd
takashi otsuka <shoshi100010000@gmail.com>
2022-08-17 18:03:39 +0900

    save_a

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -57,6 +57,9 @@ class CompaniesController < ApplicationController
     end
   end
 
+  def top
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_company
