cf70e95d128b252d4e9f09495148c7bd48da53be
takashi otsuka <shoshi100010000@gmail.com>
2022-08-17 16:53:36 +0900

    init

---
+++ db/migrate/20220817073932_create_companies.rb
@@ -0,0 +1,11 @@
+class CreateCompanies < ActiveRecord::Migration[6.1]
+  def change
+    create_table :companies do |t|
+      t.string :name
+      t.string :email
+      t.text :content
+
+      t.timestamps
+    end
+  end
+end
